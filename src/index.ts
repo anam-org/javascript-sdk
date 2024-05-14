50c46aa6517600a73f53e483dc2fc707e2f19a3c
ao-anam <alex@anam.ai>
2024-05-14 14:27:33 +0100

    webpack config

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { AnamPublicClientOptions } from './types/AnamPublicClientOptions';
 const createClient = (
   sessionToken?: string,
   options: AnamPublicClientOptions = {},
-) => {
+): AnamClient => {
   return new AnamClient(sessionToken, options);
 };
 
 const unsafe_createClientWithApiKey = (
   apiKey: string,
   options: AnamPublicClientOptions = {},
-) => {
+): AnamClient => {
   return new AnamClient(undefined, { ...options, apiKey });
 };
 
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist/umd'),
+    filename: 'anam.js',
+    library: {
+      type: 'umd',
+      name: 'anam',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+};
